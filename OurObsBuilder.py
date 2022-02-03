36be5e0965a3f7c76428195dfcba2a2b4a7bd6b6
Benjamin Chilson <benjameschilson@gmail.com>
2022-02-03 15:58:39 -0600

    inverted coordinates if on orange team

--- OurObsBuilder.py
+++ OurObsBuilder.py
@@ -1,5 +1,6 @@
 from rlgym.utils.obs_builders import ObsBuilder
 from rlgym.utils.gamestates import PlayerData, GameState
+from rlgym.utils import common_values
 import numpy as np
 
 class OurObsBuilder(ObsBuilder):
@@ -13,17 +14,27 @@ class OurObsBuilder(ObsBuilder):
 
   def build_obs(self, player: PlayerData, state: GameState, previous_action: np.ndarray):
     obs = []
-    ball_position = [round(num, 3) for num in state.ball.position.tolist()]
+
+    inverted = player.team_num == common_values.ORANGE_TEAM
+
+    if not inverted:
+      player_orientation = player.car_data
+      ball_orientation = state.ball
+    else:
+      player_orientation = player.inverted_car_data
+      ball_orientation = state.inverted_ball
+
+    ball_position = [round(num, 3) for num in ball_orientation.position.tolist()]
     obs.extend(ball_position)
-    ball_linear_velocity = [round(num, 3) for num in state.ball.linear_velocity.tolist()]
+    ball_linear_velocity = [round(num, 3) for num in ball_orientation.linear_velocity.tolist()]
     obs.extend(ball_linear_velocity)
-    player_position = [round(num, 3) for num in player.car_data.position.tolist()]
+    player_position = [round(num, 3) for num in player_orientation.position.tolist()]
     obs.extend(player_position)
-    player_yaw = round(player.car_data.yaw(), 6)
-    player_pitch = round(player.car_data.pitch(), 6)
-    player_roll = round(player.car_data.roll(), 6)
+    player_yaw = round(player_orientation.yaw(), 6)
+    player_pitch = round(player_orientation.pitch(), 6)
+    player_roll = round(player_orientation.roll(), 6)
     obs.extend([player_yaw, player_pitch, player_roll])
-    player_linear_velocity = [round(num, 3) for num in player.car_data.linear_velocity.tolist()]
+    player_linear_velocity = [round(num, 3) for num in player_orientation.linear_velocity.tolist()]
     obs.extend(player_linear_velocity)
     obs.append(round(float(player.on_ground), 3))
     obs.append(player.boost_amount)
