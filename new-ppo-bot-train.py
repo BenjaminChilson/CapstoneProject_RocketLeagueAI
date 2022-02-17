c6bcc1ffa8970372db97d104b83cd5a84218d45a
jmszq6 <jmszq6@umsystem.edu>
2022-02-17 00:01:58 -0600

    callback fix

--- new-ppo-bot-train.py
+++ new-ppo-bot-train.py
@@ -51,9 +51,9 @@ model = PPO(
     )
 
 # used to save the model after every X amount of steps
-save = CheckpointCallback(2500000, save_path="policy", name_prefix="CarBallAI")
+save = CheckpointCallback(2_500_000, save_path="policy", name_prefix="CarBallAI")
 rewardCallback = SB3CombinedLogRewardCallback(reward_names=["event_reward", "player_touch_ball", "liu_distance_ball_to_goal", "liu_distance_player_to_ball", "velocity_ball_to_goal", "velocity_player_to_goal"], file_location="combinedlogfiles")
 
 # start training, always call env.reset() before model.learn()
 env.reset()
-model.learn(total_timesteps=int(1e8), callback=[save, rewardCallback], reset_num_timesteps=False)
+model.learn(total_timesteps=int(1e8), callback=[save, SB3CombinedLogRewardCallback(reward_names=["event_reward", "player_touch_ball", "liu_distance_ball_to_goal", "liu_distance_player_to_ball", "velocity_ball_to_goal", "velocity_player_to_goal"])], reset_num_timesteps=False)
