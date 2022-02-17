8658b0cb5405133e98a31b78cd071d7db0a54ec7
jmszq6 <jmszq6@umsystem.edu>
2022-02-16 20:54:23 -0600

    reward callback fix?

--- load-ppo-bot-train.py
+++ load-ppo-bot-train.py
@@ -28,16 +28,10 @@ weighted_rewards = CombinedReward
         LiuDistanceBallToGoalReward(),
         LiuDistancePlayerToBallReward(),
         VelocityBallToGoalReward(),
-        VelocityPlayerToBallReward(),
+        VelocityPlayerToBallReward()
     ),
-    (
-        1,
-        0.4,
-        0.2,
-        0.1,
-        0.5,
-        0.2
-    )
+    
+    (1, 0.4, 0.2, 0.1, 0.5, 0.2)
 )
 
 # create the environment
