19346a826b2d6b47169a6f01dc91166a07ff66f1
jmszq6 <jmszq6@umsystem.edu>
2022-02-16 20:28:05 -0600

    reward callback fix?

--- load-ppo-bot-train.py
+++ load-ppo-bot-train.py
@@ -25,6 +25,7 @@ env = VecNormalize(env, norm_obs=False, gamma=0.995)
 
 # load the model
 model = PPO.load("policy/CarBallAI_165000000_steps.zip", env, device="auto", custom_objects=dict(n_envs=env.num_envs))
+env.reset()
 
 # used to save the model after every X amount of steps
 save = CheckpointCallback(2500000, save_path="policy", name_prefix="CarBallAI")
@@ -32,5 +33,4 @@ save = CheckpointCallback(2500000, save_path="policy", name_prefix="CarBallAI")
 rewardCallback = SB3CombinedLogRewardCallback(reward_names=["event_reward", "player_touch_ball", "liu_distance_ball_to_goal", "liu_distance_player_to_ball", "velocity_ball_to_goal", "velocity_player_to_goal"], file_location="out/reward_logs")
 
 # start training, always call env.reset() before model.learn()
-env.reset()
 model.learn(total_timesteps=int(35_000_000), callback=[save, rewardCallback], reset_num_timesteps=False)
