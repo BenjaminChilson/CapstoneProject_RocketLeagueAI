575e2eda937f411b96cae652b35dd2df4c8253c4
jmszq6 <jmszq6@umsystem.edu>
2022-02-23 04:39:01 -0600

    fix no train match

--- ppo-bot-no-train-v2.py
+++ ppo-bot-no-train-v2.py
@@ -19,37 +19,22 @@ import atexit
 from torch.nn import Tanh
 from sb3_log_reward import SB3CombinedLogReward, SB3CombinedLogRewardCallback
 
-
-# V1->V2
-# increased learning rate
-# reduced epochs
-# increased net size
-# discrete actions instead of continuous
-# obs includes other players
-# better initial rewards for the first 100M steps
-# 1v1 training against itself
-# slower transitioning of rewards
-
 if __name__ == '__main__': 
     frame_skip = 8        
     half_life_seconds = 5 
 
     fps = 120 / frame_skip
     gamma = np.exp(np.log(0.5) / (fps * half_life_seconds))
+    agents_per_match = 2
     num_instances = 1
+    target_steps = 100_000
+    steps = target_steps // (num_instances * agents_per_match)
+    batch_size = steps
 
-    # train using the 2 velocity rewards until ~100M and then start using AnnealReward to phase into other rewards
-    # need to make sure we don't drastically change rewards too quickly like we did on CarBallAI-V1
-    # ... so use AnnealReward
     def get_match():
         return Match(
             team_size=1,
             tick_skip=frame_skip,
-            self_play=False,
-            obs_builder=AdvancedObs(), 
-            state_setter=DefaultState(),
-            action_parser=DiscreteAction(),
-            game_speed=1,
             reward_function=SB3CombinedLogReward(
             (
                 VelocityPlayerToBallReward(),
@@ -71,7 +56,11 @@ if __name__ == '__main__':
                 )              
             ),
             (0.1, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0)),
-            terminal_conditions=[]
+            self_play=False,
+            terminal_conditions=[],
+            obs_builder=AdvancedObs(), 
+            state_setter=DefaultState(),
+            action_parser=DiscreteAction()
         )
 
     env = SB3MultipleInstanceEnv(get_match, num_instances)
@@ -80,10 +69,11 @@ if __name__ == '__main__':
     env = VecNormalize(env, norm_obs=False, gamma=gamma)
 
     model = PPO.load(
-            "models/CarBallAI-V2_100000000_steps.zip",
-            env,
-            device="auto",
-            custom_objects=dict(n_envs=env.num_envs))
+        "models/CarBallAI-V2_100000000_steps.zip",
+        env,
+        device="auto",
+        custom_objects=dict(n_envs=env.num_envs)
+        )
 
     run = 1
     while(run == 1):
